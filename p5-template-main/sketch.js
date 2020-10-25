b496011f217329e182ec3a5301e7032dcf9e90cb
vkm9594 <vkm9594@github.com>
2020-10-25 16:38:27 -0400

    Created a single array of objects

--- p5-template-main/sketch.js
+++ p5-template-main/sketch.js
@@ -1,33 +1,30 @@
-let ball0;
-let ball1;
+let bouncingBalls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ball0 = {
-    x: random(width),
-    y: random(50),
-    dy: 0,
-  }
-  ball1 = {
-    x: random(width),
-    y: random(50),
-    dy: 0,
-  }
+  addBalls();
 }
+
 function draw() {
   background(200);
-  ball0.y += ball0.dy;
-  ball0.dy += 0.25;
-  circle(ball0.x, ball0.y, 50);
-  ball1.y += ball1.dy;
-  ball1.dy += 0.25;
-  circle(ball1.x, ball1.y, 50);
-  // if the object hits the bottom of the canvas
-  // make it go up instead of down
-  if (ball0.y >= height && ball0.dy > 0) {
-    ball0.dy = -0.95 * ball0.dy;
-  }
-  if (ball1.y >= height && ball0.dy > 0) {
-    ball1.dy = -0.95 * ball1.dy;
+  for (ball of bouncingBalls) {
+    ball.y += ball.dy;
+    ball.dy += 0.25;
+    circle(ball.x, ball.y, 50); 
+    
+    if (ball.y >= height && ball.dy > 0) {
+      ball.dy = -0.95 * ball.dy;
+    }
+  } 
+}
+
+function addBalls() {
+ for(i = 0; i < 2; i++) {
+  balls = {
+    x: random(width),
+    y: random(50),
+    dy: 0,
+  };
+  bouncingBalls.push(balls);
   }
 }
